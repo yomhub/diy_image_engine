1fc96645625a5031ab557e4cb6925f1b4533a43d
游民 上官 <yomcoding@yomworkspace.onmicrosoft.com>
2019-06-25 10:37:15 +0900

    Adding CUDA

--- src/include/CUDA/cuimage.cu
+++ src/include/CUDA/cuimage.cu
@@ -9,6 +9,9 @@
 #define mydouble double
 #endif
 
+// threadIdx.x=N,threadIdx.y=1 col index
+// blockIdx.x=N,blockIdx.y=1 Row index
+
 __global__  void __cusmooth(std::uint8_t* dst,
 	std::uint16_t width,
 	std::uint16_t height,
@@ -20,12 +23,14 @@ __global__  void __cusmooth(std::uint8_t* dst,
 	float factor
 ) {
 	mydouble* pixelBuff = new mydouble[pixels];
-	//int row = blockIdx.y * blockDim.y + threadIdx.y;
-	//int col = blockIdx.x * blockDim.x + threadIdx.x;
-	size_t y, x, i, k, dx, dy;
-	for (y = my / 2 + my % 2; y < (height - my / 2); y++)
+	
+	size_t x = (mx/2)+threadIdx.x * threadIdx.y * ((width-(mx-1))/blockDim.x);
+	size_t y = (my/2)+blockIdx.x * blockIdx.y * ((height-(my-1))/gridDim.x);
+	size_t i, k, dx, dy;
+
+	for (dy=0; dy < ((height-(my-1))/gridDim.x); ++dy)
 	{
-		for (x = mx / 2 + mx % 2; x <= (width - mx / 2); x++) {
+		for (dx=0; dx < ((width-(mx-1))/blockDim.x); ++dx) {
 			for (i = 0; i < pixels; i++)
 			{
 				pixelBuff[i] = 0;
@@ -45,21 +50,42 @@ __global__  void __cusmooth(std::uint8_t* dst,
 	}
 }
 
-__global__  void cutest() {}
-
-void cusmooth(std::uint8_t* dst,
+CUresult cusmooth(std::uint8_t* dst,
 	std::uint16_t width,
 	std::uint16_t height,
 	std::uint8_t pixels,
 	std::uint8_t* src,
 	std::uint8_t mx,
 	std::uint8_t my,
-	mydouble* m,
-	float factor
+	mydouble* msrc,
+	float factor,
+	int device,
+	cudaDeviceProp *prop_
 ) {
+	CUresult _ret;
+	if(device<=0){
+		_ret=cudaGetDevice(&device);
+		if(device<=0)return _ret;
+		cudaGetDeviceProperties(prop_, device);
+	}
+	cudaSetDevice(device);
 
+	std::uint8_t *buff,*out;
+	mydouble* m;
 
-	__cusmooth <<<prop_.multiProcessorCount, prop_.maxThreadsPerBlock > src->height ? src->height : prop_.maxThreadsPerBlock >>> (
+	cudaMalloc(&buff, height * width * pixels);
+	cudaMalloc(&out, height * width * pixels);
+	cudaMalloc(&m, mx * my * sizeof(mydouble));
+	cudaMemcpy(buff, src, height * width * sizePerPixel, cudaMemcpyHostToDevice);
+	cudaMemcpy(m, msrc, mx * my * sizeof(mydouble), cudaMemcpyHostToDevice);
+
+	// Each block handles integer line 
+	// Each thread handles integer window
+	__cusmooth <<<prop_->multiProcessorCount >= height-(my-1) ? height-(my-1) : prop_->multiProcessorCount, 
+	prop_->maxThreadsPerBlock >= width-(mx-1)  ? width-(mx-1) : prop_.maxThreadsPerBlock >>> (
 		out,src->width, src->height, src->sizePerPixel,buff,mask->x, mask->y, m,factor);
-
+	cudaMemcpy(src,out,height * width * sizePerPixel, cudaMemcpyDeviceToHost);
+	cudeFree(buff);
+	cudeFree(out);
+	cudeFree(m);
 }
